7f82d576dedb7b890616e51f43edaee95c3d2910
Rob Slavin <rob@slavin.com.au>
2024-03-24 21:43:56 +1100

    Feedback

--- src/FundingSolver.fs
+++ src/FundingSolver.fs
@@ -6,62 +6,76 @@ open Feliz.UseElmish
 
 module FundingSolver =
 
+  type LineItemResult = {
+    Id: string
+    Qty: int
+  }
+
+  type SolverResult =
+    | Success of (LineItemResult * LineItemResult * LineItemResult) list
+    | InProgress
+    | NoSolutionsFound
+    | Error of string
+    | NotStarted
+
   type State = {
     TotalCost: string
     ``11_022``: LineItem
     ``11_023``: LineItem
     ``11_024``: LineItem
-    Result: (LineItem * LineItem * LineItem) list option
+    Result: SolverResult
   }
   and LineItem = {
     Id: string
     Price: float
-    Qty: int 
   }
 
   type Msg =
     | UpdateLineItemCost of string * float
-    | UpdateLineItemQty of string * int
     | UpdateTotalCost of string
     | Solve
+    | Solved of SolverResult
 
   module State =
+
+    let [<Literal>] MaxQty = 500
     
     let init () =
-      { 
-        TotalCost = "17153.28"
-        ``11_022`` = { Id = "11_022"; Price = 214.41; Qty = 60  }
-        ``11_023`` = { Id = "11_023"; Price = 193.99; Qty = 40  }
-        ``11_024`` = { Id = "11_024"; Price = 74.63;  Qty = 200 }
-        Result = None
+      { TotalCost = ""
+        ``11_022`` = { Id = "11_022"; Price = 214.41 }
+        ``11_023`` = { Id = "11_023"; Price = 193.99 }
+        ``11_024`` = { Id = "11_024"; Price = 74.63 }
+        Result = NotStarted
       }, Cmd.none
 
 
-    let solve (state: State) =
+    let solve (state: State) = async {
       printfn "Solving..."
-      try
-        let _022 = state.``11_022``
-        let _023 = state.``11_023``
-        let _024 = state.``11_024``
-        let totalCost = state.TotalCost |> float
 
-        [ 
-          for x in [0.._022.Qty] do
-          for y in [0.._023.Qty] do
-          for z in [0.._024.Qty] do
-            if (_022.Price * (float x)) + (_023.Price * (float y)) + (_024.Price * (float z)) = totalCost then
-              yield ( 
-                { Id = "11_022"; Price = _022.Price; Qty = x },
-                { Id = "11_023"; Price = _023.Price; Qty = y },
-                { Id = "11_024"; Price = _024.Price; Qty = z }
-              )
-        ] |> (fun x -> printfn "Result: %A" x; x)
+      let _022 = state.``11_022``
+      let _023 = state.``11_023``
+      let _024 = state.``11_024``
 
-      with ex ->
-        printfn "Error: %s" ex.Message
-        []
+      match System.Double.TryParse(state.TotalCost) with
+      | (true, totalCost) ->
+          return
+            [ for x in [0..MaxQty] do
+              for y in [0..MaxQty] do
+              for z in [0..MaxQty] do
+                if (_022.Price * (float x)) + (_023.Price * (float y)) + (_024.Price * (float z)) = totalCost then
+                  yield ( 
+                    { Id = "11_022"; Qty = x },
+                    { Id = "11_023"; Qty = y },
+                    { Id = "11_024"; Qty = z }
+                  )
+            ] |> function
+            | [] -> NoSolutionsFound
+            | x -> Success x
+      | (false, _) -> return Error "Total cost is not a valid number"
+    }
 
-    let update : Msg -> State -> State * Cmd<'a> =
+
+    let update : Msg -> State -> State * Cmd<Msg> =
       fun (msg: Msg) (state: State) ->
         match msg with
         | UpdateLineItemCost (id, price) ->
@@ -72,19 +86,13 @@ module FundingSolver =
               | "11_024" -> { state with ``11_024`` = { state.``11_024`` with Price = price } }
               | _ -> failwith "Invalid line item id"
             newState, Cmd.none
-        | UpdateLineItemQty (id, qty) ->
-            let newState = 
-              match id with
-              | "11_022" -> { state with ``11_022`` = { state.``11_022`` with Qty = qty } }
-              | "11_023" -> { state with ``11_023`` = { state.``11_023`` with Qty = qty } }
-              | "11_024" -> { state with ``11_024`` = { state.``11_024`` with Qty = qty } }
-              | _ -> failwith "Invalid line item id"
-            newState, Cmd.none
         | UpdateTotalCost tc ->
             { state with TotalCost = tc }, Cmd.none
         | Solve ->
-            let result = solve state
-            { state with Result = Some result }, Cmd.none
+            let solveAsync () = state |> solve
+            { state with Result = InProgress }, Cmd.OfAsync.perform solveAsync () Solved
+        | Solved result ->
+            { state with Result = result }, Cmd.none
 
   let logo =
     Html.img [ 
@@ -127,39 +135,35 @@ module FundingSolver =
       ]
     ]
 
-  let qtyInput dispatch itemId (item: LineItem) =
-    Html.div [
-      prop.className "relative col-span-3 col-start-6 mt-2 rounded-md shadow-sm"
-      prop.children [
-        Html.input [
-          prop.type' "number"
-          prop.name "qty"
-          prop.id "qty"
-          prop.className "block w-full rounded-md border-0 py-1.5 pl-3 pr-12 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          //prop.placeholder "100"
-          prop.min 0
-          prop.max 999
-          prop.maxLength 3
-          prop.value (string item.Qty)
-          prop.onChange (fun x -> UpdateLineItemQty(itemId, x) |> dispatch)
-        ]
-        Html.div [
-          prop.className "pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3"
-          prop.children [ Html.span [ prop.className "text-gray-500 sm:text-sm"; prop.id "price-currency"; prop.text "QTY" ] ]
-        ]
-      ]
-    ]
-
-  let lineItem dispatch (item: LineItem) =
+  let lineItemInput dispatch { Id=itemId; Price=itemPrice } =
     Html.div [
       prop.className "mt-2"
       prop.children [
-        Html.label [ prop.className "block text-sm font-medium leading-6 text-gray-900"; prop.for' "price"; prop.text item.Id ]
+        Html.label [ prop.className "block text-sm font-medium leading-6 text-gray-900"; prop.for' "total"; prop.text itemId ]
         Html.div [
-          prop.className "grid grid-cols-8 gap-x-4"
+          prop.className "relative rounded-md shadow-sm"
           prop.children [
-            priceInput dispatch item.Id item.Price
-            qtyInput dispatch item.Id item
+            Html.div [
+              prop.classes [ "pointer-events-none"; "absolute"; "inset-y-0"; "left-0"; "flex"; "items-center"; "pl-3" ]
+              prop.children [ Html.span [ prop.classes [ "text-gray-500"; "sm:text-sm" ]; prop.text "$" ] ]
+            ]
+            Html.input [
+              prop.type' "number"
+              prop.name "total"
+              prop.id "total"
+              prop.className "block w-full rounded-md border-0 py-1.5 pl-7 pr-12 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              prop.placeholder "Total cost"
+              prop.onTextChange (fun newCost -> (itemId, float newCost) |> UpdateLineItemCost |> dispatch)
+              prop.value itemPrice
+              prop.min 0.01
+              prop.step 0.01
+              prop.max 99999.99
+              prop.maxLength 8
+            ]
+            Html.div [
+              prop.classes [ "pointer-events-none"; "absolute"; "inset-y-0"; "right-0"; "flex"; "items-center"; "pr-3" ]
+              prop.children [ Html.span [ prop.classes [ "text-gray-500"; "sm:text-sm" ]; prop.id "price-currency"; prop.text "AUD" ] ]
+            ]
           ]
         ]
       ]
@@ -200,17 +204,26 @@ module FundingSolver =
     ]
     
 
-  let button (label: string) onClick =
+  let button (label: string) isPrimary isWaiting onClick =
+    let spinner = Html.span [ prop.text "Working..." ]
+
     Html.button [
       prop.className [
-        "flex w-full mt-8 justify-center rounded-md bg-sky-600 px-3 py-1.5 text-base font-semibold"
-        "leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2"
-        "focus-visible:outline-offset-2 focus-visible:outline-sky-600 select-none" ]
+        "flex w-full mt-4 justify-center rounded-md px-3 py-1.5 text-base font-semibold"
+        "leading-6 shadow-sm focus-visible:outline focus-visible:outline-2"
+        "focus-visible:outline-offset-2 select-none"
+        if isPrimary
+        then "bg-sky-600 text-white hover:bg-sky-500 focus-visible:outline-sky-600"
+        else "bg-gray-200 text-gray-800 hover:bg-gray-300 focus-visible:outline-gray-200" ]
       prop.text label
-      prop.onClick onClick
+      prop.onClick <| if isWaiting then (fun _ -> ()) else onClick
+      prop.disabled isWaiting
+      prop.children [
+        if isWaiting then spinner else Html.span [ prop.text label]
+      ]
     ]
 
-  let result (resultOpt:(LineItem * LineItem * LineItem) list option) =
+  let result (result: SolverResult) =
 
     let listItem (name: string) (value: string) =
       Html.li [
@@ -242,14 +255,17 @@ module FundingSolver =
       listItem "11_024" $"%d{z.Qty}"
     ]
 
-    match resultOpt with
-    | Some result ->
+    match result with
+    | InProgress -> Html.none
+    | NoSolutionsFound -> Html.div [ prop.className "mt-6"; prop.text "No solutions found" ]
+    | Error msg -> Html.div [ prop.className "mt-6"; prop.text msg ]
+    | NotStarted -> Html.none
+    | Success resultLst ->
         Html.ul [ 
           prop.className "grid grid-cols-3 gap-6 mt-6 border-1 border-blue-100 rounded-lg bg-gray-50 shadow"
           prop.role "list"
-          prop.children (result |> List.collect createRow)
+          prop.children (resultLst |> List.collect createRow)
         ]
-    | None -> Html.div []
 
 
   let page dispatch (state: State) =
@@ -263,18 +279,19 @@ module FundingSolver =
           prop.classes [ "relative"; "bg-white"; "px-6"; "pb-8"; "pt-10"; "shadow-xl"; "ring-1"; "ring-gray-900/5"; "sm:mx-auto"; "sm:max-w-lg"; "sm:rounded-lg"; "sm:px-10" ]
           prop.children [
             Html.div [
-              prop.className "mx-auto max-w-md"
+              prop.className "mx-auto max-w-md sm:w-96 "
               prop.children [
                 Html.h1 [ prop.className "mb-8 text-3xl font-extrabold text-gray-900"; prop.text "Funding Solver" ]
                 Html.div [
                   prop.children [
                     Html.div [
-                      lineItem dispatch state.``11_022``
-                      lineItem dispatch state.``11_023``
-                      lineItem dispatch state.``11_024``
+                      lineItemInput dispatch state.``11_022``
+                      lineItemInput dispatch state.``11_023``
+                      lineItemInput dispatch state.``11_024``
                     ]
                     totalInput dispatch state.TotalCost
-                    button "Solve" (fun _ -> Solve |> dispatch)
+                    button "Example" false false (fun _ -> UpdateTotalCost "17153.28" |> dispatch)
+                    button "Solve" true (state.Result = InProgress) (fun _ -> Solve |> dispatch)
                     result state.Result
                   ]
                 ]
